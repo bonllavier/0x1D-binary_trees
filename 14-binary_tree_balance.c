1696fde59abeac0f11f54f51c6d67306230f5453
javb92 <mentor37_@hotmail.com>
2019-07-24 22:28:45 +0000

    [COMPLETED]

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,49 +1,42 @@
 #include "binary_trees.h"
 /**
- *binary_tree_depth - depth
+ *binary_tree_balance - check balance
  *@tree: tree.
  *Return: nothing.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int ladol = 0;
-	int lador = 0;
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	ladol = height_2(tree->left);
-	lador = height_2(tree->right);
-	printf("%d ladol\n", ladol);
-	printf("%d lador\n", lador);
-	return (ladol - lador);
+	return (height_2(tree->left) - height_2(tree->right));
 }
-
-int height_2(const binary_tree_t *tree)
+/**
+ *height_2 - depth
+ *@tree: tree.
+ *Return: nothing.
+ */
+size_t height_2(const binary_tree_t *tree)
 {
-        int heightl = 0;
-        int heightr = 0;
-        int height = 0;
+	size_t heightl = 0;
+	size_t heightr = 0;
 
-        if (tree == NULL)
-        {
-                return (0);
-        }
-        else
-        {
-                heightl = height_2(tree->left);
-                heightr = height_2(tree->right);
-                if (heightl > heightr)
-                {
-                        return (heightl + 1);
-                }
-                else if (tree->left != NULL || tree->right != NULL)
-                {
-                        return (heightr + 1);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		heightl = height_2(tree->left);
+		heightr = height_2(tree->right);
+		if (heightl > heightr)
+		{
+			return (heightl + 1);
 		}
-                else
-                {
-                        return (height);
-                }
-        }
+		else
+		{
+			return (heightr + 1);
+		}
+	}
 }
