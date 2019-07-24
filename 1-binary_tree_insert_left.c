0fc4a038440347d1bf6aebb3b60ba155c13da5ea
javb92 <mentor37_@hotmail.com>
2019-07-24 21:16:05 +0000

    [DELETED] redundant lines of code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,18 +16,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	node = binary_tree_node(parent, value);
 	if (parent->left == NULL)
 	{
-		node->n = value;
-		node->parent = parent;
-		node->left = NULL;
-		node->right = NULL;
 		parent->left = node;
 	}
 	else
 	{
-		node->n = value;
-		node->parent = parent;
 		node->left = parent->left;
-		node->right = NULL;
 		parent->left->parent = node;
 		parent->left = node;
 	}
